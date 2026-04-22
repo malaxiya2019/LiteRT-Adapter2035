c905d57b19b856fca5f10e2436098d0f13a97fdd
malaxiya2019 <40144223+malaxiya2019@users.noreply.github.com>
2026-04-22 18:36:44 +0800

    Add MainActivity with basic setup and log message

--- app/src/main/java/com/example/litert/MainActivity.kt
+++ app/src/main/java/com/example/litert/MainActivity.kt
@@ -1,1 +1,13 @@
-#
+package com.example.litert
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        println("LiteRT Adapter Running")
+    }
+}
